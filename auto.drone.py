2127a041eab761f5526ccd2459a7311266ecad34
Yevhenii Doroshenko <doroshpoison@gmail.com>
2025-12-07 13:19:26 +0200

    Simplify planting and harvesting process
    Refactor the planting and harvesting logic for better efficiency.

--- auto.drone.py
+++ auto.drone.py
@@ -1,42 +1,24 @@
-import main
-
 while True:
-    get_tick_count()
-
+  for i in range(get_world_size()):
+    harvest()
+    plant(Entities.Pumpkin)
+    
+    if i < get_world_size() - 1:
+      move(East)
+  
+  move(North)
+  
+  for i in range(get_world_size()):
+    harvest()
     plant(Entities.Carrot)
-    if can_harvest():
-        harvest()
-        move(North)
-        move(East)
     use_item(Items.Water)
-
-    plant(Entities.Bush)
-    if can_harvest():
-        harvest()
-        move(North)
-        move(East)
-        if get_entity_type() == Entities.Bush:
-            do_a_flip()
-
-
-def is_even(n):
-    return n % 2 == 0
-
-
+    
+    if i < get_world_size() - 1:
+      move(West)
+  
+  move(North)
+  
 while True:
-    if till():
-        move(North)
-    move(East)
-
-# Основний цикл
-while True:
-    get_tick_count()
-
-    # Спочатку обробляємо ґрунт
-    till_and_move()
-
-    # Потім садимо та збираємо
-    plant_and_harvest_cycle()
-
-    # Переміщуємось далі
-    move_pattern()
+  if till():
+    move(North)
+  move(East)
